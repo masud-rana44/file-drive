9eab6f30d62e9ae1ebe5a0f571a9fd125b0e5022
masud-rana44 <mdmasudrana4488@gmail.com>
2026-01-22 20:20:39 +0600

    adding a view toggle with a table view

--- src/app/dashboard/_components/file-browser.tsx
+++ src/app/dashboard/_components/file-browser.tsx
@@ -4,12 +4,15 @@ import { api } from "@/convex/_generated/api";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 
-import { Loader2 } from "lucide-react";
+import { GridIcon, Loader2, RowsIcon } from "lucide-react";
 import Image from "next/image";
 import UploadButton from "./upload-button";
 import FileCard from "./file-card";
 import { useState } from "react";
 import { SearchBar } from "./search-bar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DataTable from "./file-table";
+import { columns } from "./columns";
 
 function Placeholder() {
   return (
@@ -55,38 +58,56 @@ function FileBrowser({
   );
   const isLoading = files === undefined;
 
+  const modifiedFiles =
+    files?.map((file) => ({
+      ...file,
+      isFavorited: (favorites ?? []).some(
+        (favorite) => favorite.fileId === file._id,
+      ),
+    })) ?? [];
+
   return (
     <div>
-      {isLoading && (
-        <div className="flex flex-col gap-8 w-full items-center mt-24">
-          <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
-          <div className="text-2xl">Loading your files...</div>
-        </div>
-      )}
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-bold">{tittle}</h1>
 
-      {!isLoading && (
-        <>
-          <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold">{tittle}</h1>
+        <SearchBar setQuery={setQuery} />
 
-            <SearchBar setQuery={setQuery} />
+        <UploadButton />
+      </div>
 
-            <UploadButton />
+      <Tabs defaultValue="grid">
+        <TabsList className="mb-2">
+          <TabsTrigger value="grid" className="flex gap-2 items-center">
+            <GridIcon />
+            Grid
+          </TabsTrigger>
+          <TabsTrigger value="table" className="flex gap-2 items-center">
+            <RowsIcon /> Table
+          </TabsTrigger>
+        </TabsList>
+
+        {isLoading && (
+          <div className="flex flex-col gap-8 w-full items-center mt-24">
+            <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+            <div className="text-2xl">Loading your files...</div>
           </div>
+        )}
 
-          {files.length === 0 && <Placeholder />}
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-            {files?.map((file) => (
-              <FileCard
-                key={file._id}
-                file={file}
-                favorites={favorites ?? []}
-              />
-            ))}
+        <TabsContent value="grid">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {modifiedFiles?.map((file) => {
+              return <FileCard key={file._id} file={file} />;
+            })}
           </div>
-        </>
-      )}
+        </TabsContent>
+        <TabsContent value="table">
+          {" "}
+          <DataTable columns={columns} data={modifiedFiles} />
+        </TabsContent>
+      </Tabs>
+
+      {files?.length === 0 && <Placeholder />}
     </div>
   );
 }
--- src/app/dashboard/_components/file-card.tsx
+++ src/app/dashboard/_components/file-card.tsx
@@ -5,159 +5,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { formatRelative } from "date-fns";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Doc, Id } from "@/convex/_generated/dataModel";
-import {
-  FileIcon,
-  FileTextIcon,
-  GanttChartIcon,
-  ImageIcon,
-  MoreVertical,
-  StarHalf,
-  StarIcon,
-  Trash2Icon,
-  UndoIcon,
-} from "lucide-react";
-import { ReactNode, useState } from "react";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { toast } from "sonner";
+import { formatRelative } from "date-fns";
+
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { FileTextIcon, GanttChartIcon, ImageIcon } from "lucide-react";
+import { ReactNode } from "react";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
 import Image from "next/image";
-import { Protect } from "@clerk/nextjs";
+import FileCardActions, { getFileUrl } from "./file-actions";
 
-function FileCardActions({
+export default function FileCard({
   file,
-  isFavorited,
 }: {
-  file: Doc<"files">;
-  isFavorited: boolean;
-}) {
-  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
-  const deleteFile = useMutation(api.files.deleteFile);
-  const toggleFavorite = useMutation(api.files.toggleFavorite);
-  const restoreFile = useMutation(api.files.restoreFile);
-
-  async function handleDeleteFile() {
-    try {
-      await deleteFile({
-        fileId: file._id,
-      });
-
-      toast.success("File deleted successfully.");
-    } catch {
-      toast.error("Failed to delete file. Please try again.");
-    }
-  }
-
-  return (
-    <>
-      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action will mark the file for our deletion process. Files are
-              deleted periodically
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={handleDeleteFile}>
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <MoreVertical />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            onClick={() => {
-              window.open(getFileUrl(file.fileId), "_blank");
-            }}
-            className="flex gap-1 items-center cursor-pointer"
-          >
-            <FileIcon className="w-4 h-4" /> Download
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            onClick={() => toggleFavorite({ fileId: file._id })}
-            className="flex gap-1 items-center cursor-pointer"
-          >
-            {isFavorited ? (
-              <div className="flex gap-1 items-center">
-                <StarIcon className="h-4 w-4" /> Unfavorite
-              </div>
-            ) : (
-              <div className="flex gap-1 items-center">
-                <StarHalf className="h-4 w-4" /> Favorite
-              </div>
-            )}
-          </DropdownMenuItem>
-          <Protect role="org:admin" fallback={null}>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={() => {
-                if (file.shouldDelete) {
-                  restoreFile({
-                    fileId: file._id,
-                  });
-                } else {
-                  setIsConfirmOpen(true);
-                }
-              }}
-              className="flex gap-1 items-center cursor-pointer"
-            >
-              {file.shouldDelete ? (
-                <div className="flex gap-1 text-green-600 items-center cursor-pointer">
-                  <UndoIcon /> <span>Restore</span>
-                </div>
-              ) : (
-                <div className="flex gap-1 text-red-600 items-center cursor-pointer">
-                  <Trash2Icon /> <span>Delete</span>
-                </div>
-              )}
-            </DropdownMenuItem>
-          </Protect>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
-  );
-}
-
-function getFileUrl(fileId: Id<"_storage">): string {
-  console.log(" File Id: --->>> ", fileId);
-  return `${process.env.NEXT_PUBLIC_CONVEX_URL}/api/storage/9bfbe50a-3888-472c-82aa-9bf0dd786cd0`;
-  return `${process.env.NEXT_PUBLIC_CONVEX_URL}/api/storage/${fileId}`;
-}
-
-function FileCard({
-  file,
-  favorites,
-}: {
-  file: Doc<"files">;
-  favorites: Doc<"favorites">[];
+  file: Doc<"files"> & { isFavorited: boolean };
 }) {
   const userProfile = useQuery(api.users.getUserProfile, {
     userId: file.userId,
@@ -169,22 +31,18 @@ function FileCard({
     csv: <GanttChartIcon />,
   } as Record<Doc<"files">["type"], ReactNode>;
 
-  const isFavorited = favorites.some(
-    (favorite) => favorite.fileId === file._id,
-  );
-
   return (
     <Card>
       <CardHeader className="relative">
-        <CardTitle className="flex items-center gap-2">
-          <div className="flex justify-center">{typeIcons[file.type]}</div>
+        <CardTitle className="flex gap-2 text-base font-normal">
+          <div className="flex justify-center">{typeIcons[file.type]}</div>{" "}
           {file.name}
         </CardTitle>
         <div className="absolute top-2 right-2">
-          <FileCardActions isFavorited={isFavorited} file={file} />
+          <FileCardActions isFavorited={file.isFavorited} file={file} />
         </div>
       </CardHeader>
-      <CardContent>
+      <CardContent className="h-50 flex justify-center items-center">
         {file.type === "image" && (
           <Image
             alt={file.name}
@@ -197,17 +55,14 @@ function FileCard({
         {file.type === "csv" && <GanttChartIcon className="w-20 h-20" />}
         {file.type === "pdf" && <FileTextIcon className="w-20 h-20" />}
       </CardContent>
-      <CardFooter>
+      <CardFooter className="flex justify-between">
         <div className="flex gap-2 text-xs text-gray-700 w-40 items-center">
-          <Avatar>
+          <Avatar className="w-6 h-6">
             <AvatarImage src={userProfile?.image} />
-            <AvatarFallback>
-              {userProfile?.name?.slice(0, 2).toUpperCase()}
-            </AvatarFallback>
+            <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <span>{userProfile?.name}</span>
+          {userProfile?.name}
         </div>
-
         <div className="text-xs text-gray-700">
           Uploaded on {formatRelative(new Date(file._creationTime), new Date())}
         </div>
@@ -215,5 +70,3 @@ function FileCard({
     </Card>
   );
 }
-
-export default FileCard;
